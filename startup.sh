be34a22453366f268f85382c4bd0dbdbf40a6e6e
Ntkz <142138719+ntokozomas@users.noreply.github.com>
2025-06-23 20:20:49 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# -------------------------------------------
+# --------------------------------------------------
 # iFundo Startup Script
-# -------------------------------------------
+# --------------------------------------------------
 
-echo "[$(date)] Starting iFundo initialization..." >> startup.log
+LOGFILE="startup.log"
+VENV_PATH="/home/pi/ifundo/venv"      
+APP_PATH="/home/pi/ifundo/start_ifundo.py"  
 
+echo "[$(date)] Starting iFundo initialization..." >> $LOGFILE
 
-echo "[$(date)] Checking virtual environment..." >> startup.log
+echo "[$(date)] Checking virtual environment..." >> $LOGFILE
 sleep 2
 
-
-echo "[$(date)] Activating virtual environment..." >> startup.log
+echo "[$(date)] Activating virtual environment..." >> $LOGFILE
+source "$VENV_PATH/bin/activate"
 sleep 2
 
-# dependency check
-echo "[$(date)] Verifying dependencies..." >> startup.log
+echo "[$(date)] Verifying dependencies..." >> $LOGFILE
+pip list >> $LOGFILE
 sleep 2
 
-# application startup
-echo "[$(date)] Launching iFundo application..." >> startup.log
+echo "[$(date)] Launching iFundo application..." >> $LOGFILE
+python3 "$APP_PATH" >> $LOGFILE 2>&1 &
 sleep 2
 
-# Done
-echo "[$(date)] iFundo startup sequence complete." >> startup.log
-
+echo "[$(date)] iFundo startup sequence complete." >> $LOGFILE
 exit 0
